eb08a832d104314a4ee874a335979fc437ed3e85
adarshrajput99 <68708958+adarshrajput99@users.noreply.github.com>
2023-04-06 19:38:29 +0530

    call a day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,10 @@
 
 namespace App\Console;
 
+use App\Http\Controllers\copy;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Sykez\CronlessScheduler\CronlessSchedule;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -12,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        //$schedule->command('copy::class@index')->everyMinute();
     }
 
     /**
@@ -24,4 +25,5 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+    
 }
--- app/Http/Controllers/copy.php
+++ app/Http/Controllers/copy.php
@@ -7,13 +7,19 @@ use Illuminate\Support\Facades\DB;
 class copy extends Controller
 {
 function index(){
+    $start = hrtime(true); // Record the start time
     $offset=DB::connection('mysql2')->table('watchdogs')->count();
-    $from=DB::connection('mysql')->select('select * from watchdog limit 1000 offset '.$offset);
+    $from=DB::connection('mysql')->select('select * from watchdog limit 4000 offset '.$offset);
     
     foreach($from as $field ){
         
         DB::connection('mysql2')->table('watchdogs')->insert((array)$field);
     }
+    $end = hrtime(true); // Record the end time
+    $duration = ($end - $start) / 1e+9; // Calculate the duration in milliseconds
+    $offset=DB::connection('mysql2')->table('watchdogs')->count();
     echo $offset;
+    echo " records are done  and Time taken: {$duration} sec ";
+
 }
 }
