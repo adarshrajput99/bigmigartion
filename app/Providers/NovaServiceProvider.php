a64f57d1fd6086f4187cae552ed709a85f58b9c2
Adarsh Singh <99adarshsingh27@gmail.com>
2023-04-26 06:08:16 +0000

    26 Apr 2023, 11:38

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -92,7 +92,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function dashboards()
     {
         return [
-            new \App\Nova\Dashboards\Main,
+            new \App\Nova\Dashboards\executive,
         ];
     }
 
