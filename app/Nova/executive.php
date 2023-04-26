bf188573a0071d02e4b7f2521773f12fd39b06e0
Adarsh Singh <99adarshsingh27@gmail.com>
2023-04-26 06:51:24 +0000

    26 Apr 2023, 12:21

--- app/Nova/executive.php
+++ app/Nova/executive.php
@@ -3,8 +3,20 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use HasRoles;
+use Sereny\NovaPermissions\Fields\Checkboxes;
+use Silvanite\NovaToolPermissions\Role;
+use Laravel\Nova\Fields\MorphToMany;
+use App\Nova\lenses\MyLens;
+use Laravel\Nova\Fields\Select;
+
 
 class executive extends Resource
 {
@@ -41,6 +53,27 @@ class executive extends Resource
     {
         return [
             ID::make()->sortable(),
+
+            Gravatar::make()->maxWidth(50),
+
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Email')
+                ->sortable()
+                ->rules('required', 'email', 'max:254')
+                ->creationRules('unique:users,email')
+                ->updateRules('unique:users,email,{{resourceId}}'),
+
+            Password::make('Password')
+                ->onlyOnForms()
+                ->creationRules('required', Rules\Password::defaults())
+                ->updateRules('nullable', Rules\Password::defaults()),
+                #MorphToMany::make('Roles', 'roles', \Sereny\NovaPermissions\Nova\Role::class),
+                #MorphToMany::make('Permissions', 'permissions', \Sereny\NovaPermissions\Nova\Permission::class),
+
+                
         ];
     }
 
